8a890eaf6f4543495d887d387b706bbab59758b2
Marcos Castro <102087019+marcoscastroj@users.noreply.github.com>
2022-05-24 00:22:32 -0300

    Final Version of P. Ombudsman

--- main.py
+++ main.py
@@ -1,52 +1,52 @@
-import funcoes
+from funcoes import *
 
-opcao = -1
+option = -1
 while True:
-    print('''Ouvidoria Universidade ABC:
-    1) Listar todas as manifestações 
-    2) Listar todas as sugestões
-    3) Listar todas as reclamações
-    4) Listar todos os elogios
-    5) Enviar uma manifestação (criar uma nova)
-    6) Pesquisar protocolo por número (ID)
-    7) Sair''')
+    print('''ABC University Ombudsman:
+    1) List all manifestations
+    2) List all suggestions
+    3) List all questions
+    4) List all compliments
+    5) Submit a manifestation (create a new one)
+    6) Search protocol by number (ID)
+    7) Leave''')
     while True:
         try:
-            opcao = int(input('Digite uma opção: '))
-            if opcao != 1 and opcao != 2 and opcao != 3 and opcao != 4 and opcao != 5 and opcao != 6 and opcao != 7:
-                opcao = int(input('Digite uma opção valida: '))
+            option = int(input('Enter an option: '))
+            if option != 1 and option != 2 and option != 3 and option != 4 and option != 5 and option != 6 and option != 7:
+                option = int(input('Digite uma opção valida: '))
         except(ValueError, TypeError):
-            print('Digite uma opção valida! ')
+            print('Please enter a valid option! ')
             continue
         else:
-            print(f'Apresentando a opção: {opcao}')
+            print(f'Introducing the option: {option}')
             break
 
-    #   OPÇÃO PARA FINALIZAR O SISTEMA
-    if opcao == 7:
-        print('Fim do Programa!')
+    # OPTION TO END THE SYSTEM
+    if option == 7:
+        print('End of Program!')
         break
 
-    #   OPÇÃO PARA CRIAR UMA MANIFESTAÇÃO
-    elif opcao == 5:
-        funcoes.opcao_5()
+    # OPTION TO CREATE A MANIFESTATION
+    elif option == 5:
+        createManifestation()
 
-    #   OPÇÃO PARA LISTAR AS MANIFESTAÇÕES
-    elif opcao == 1:
-        funcoes.opcao_1()
+    # OPTION TO LIST THE MANIFESTATIONS
+    elif option == 1:
+        listDemonstrations()
 
-    #   OPÇÃO PARA LISTAR AS SUGESTOES
-    elif opcao == 2:
-        funcoes.opcao_2()
+    # OPTION TO LIST THE SUGGESTIONS
+    elif option == 2:
+        listSuggestions()
 
-    #   OPÇÃO PARA LISTAR AS RECLAMAÇÕES
-    elif opcao == 3:
-        funcoes.opcao_3()
+    # OPTION TO LIST THE COMPLAINTS
+    elif option == 3:
+        listComplaints()
 
-    #   OPÇÃO PARA LISTAR OS ELOGIOS
-    elif opcao == 4:
-        funcoes.opcao_4()
+    # OPTION TO LIST THE COMPLIMENTS
+    elif option == 4:
+        listPraise()
 
-    #   OPÇÃO PARA MOSTRAR O PROTOCOLO
-    elif opcao == 6:
-        funcoes.opcao_6()
+    # OPTION TO SEARCH BY PROTOCOL
+    elif option == 6:
+        searchProtocol()
